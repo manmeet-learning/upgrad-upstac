7791c9c5c6c99e4b4db4187b2f346a8a228c0990
manmeet-learning <82712974+manmeet-learning@users.noreply.github.com>
2021-04-17 20:55:32 +0530

    Updated ConsultationController & LabRequestController.
    1. Implemented getForTester in LabRequestController for get all tests which are assigned to logged-in tester.
    2. Implemented getForConsultations, getForDoctor, assignForConsultation & updateConsultation in ConsultationController.

--- src/main/java/org/upgrad/upstac/testrequests/consultation/ConsultationController.java
+++ src/main/java/org/upgrad/upstac/testrequests/consultation/ConsultationController.java
@@ -10,10 +10,12 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import org.upgrad.upstac.config.security.UserLoggedInService;
 import org.upgrad.upstac.exception.AppException;
+import org.upgrad.upstac.testrequests.RequestStatus;
 import org.upgrad.upstac.testrequests.TestRequest;
 import org.upgrad.upstac.testrequests.TestRequestQueryService;
 import org.upgrad.upstac.testrequests.TestRequestUpdateService;
 import org.upgrad.upstac.testrequests.flow.TestRequestFlowService;
+import org.upgrad.upstac.users.User;
 
 import javax.validation.ConstraintViolationException;
 import java.util.List;
@@ -27,9 +29,6 @@ import static org.upgrad.upstac.exception.UpgradResponseStatusException.asConstr
 public class ConsultationController {
 
     Logger log = LoggerFactory.getLogger(ConsultationController.class);
-
-
-
 
     @Autowired
     private TestRequestUpdateService testRequestUpdateService;
@@ -45,92 +44,56 @@ public class ConsultationController {
     private UserLoggedInService userLoggedInService;
 
 
-
+    //Get all tests which are due for consultation and not assigned to any doctor.
     @GetMapping("/in-queue")
     @PreAuthorize("hasAnyRole('DOCTOR')")
     public List<TestRequest> getForConsultations()  {
 
-        // Implement this method
-
-
-        //Implement this method to get the list of test requests having status as 'LAB_TEST_COMPLETED'
-        // make use of the findBy() method from testRequestQueryService class
-        //return the result
-        // For reference check the method getForTests() method from LabRequestController class
-
-        // replace this line of code with your implementation
-        throw new ResponseStatusException(HttpStatus.NOT_IMPLEMENTED,"Not implemented");
-
-
+        //Fetch and return all tests which are due for consultation and not assigned to any doctor.
+        return testRequestQueryService.findBy(RequestStatus.LAB_TEST_COMPLETED);
     }
 
+    //Get all tests which are assigned to logged-in doctor.
     @GetMapping
     @PreAuthorize("hasAnyRole('DOCTOR')")
     public List<TestRequest> getForDoctor()  {
 
-        //Implement this method
+        //Get logged-in user(doctor) related details.
+        User doctor=userLoggedInService.getLoggedInUser();
 
-        // Create an object of User class and store the current logged in user first
-        //Implement this method to return the list of test requests assigned to current doctor(make use of the above created User object)
-        //Make use of the findByDoctor() method from testRequestQueryService class to get the list
-        // For reference check the method getForTests() method from LabRequestController class
-
-        // replace this line of code with your implementation
-        throw new ResponseStatusException(HttpStatus.NOT_IMPLEMENTED,"Not implemented");
-
-
-
-
+        //Return list of tests which are assigned to logged-in user(doctor).
+        return testRequestQueryService.findByDoctor(doctor);
     }
 
-
-
+    //Assign test to logged in doctor & return test details.
     @PreAuthorize("hasAnyRole('DOCTOR')")
     @PutMapping("/assign/{id}")
     public TestRequest assignForConsultation(@PathVariable Long id) {
-
-        // Implement this method
-
-        // Implement this method to assign a particular test request to the current doctor(logged in user)
-        //Create an object of User class and get the current logged in user
-        //Create an object of TestRequest class and use the assignForConsultation() method of testRequestUpdateService to assign the particular id to the current user
-        // return the above created object
-        // For reference check the method assignForLabTest() method from LabRequestController class
         try {
-            // replace this line of code with your implementation
-            throw new ResponseStatusException(HttpStatus.NOT_IMPLEMENTED,"Not implemented");
+            //Get logged-in user(doctor) related details.
+            User doctor =userLoggedInService.getLoggedInUser();
 
+            //Assign test to logged in doctor & return test details.
+            return testRequestUpdateService.assignForConsultation(id,doctor);
         }catch (AppException e) {
             throw asBadRequest(e.getMessage());
         }
     }
 
-
-
+    //Update consultation details & return test details.
     @PreAuthorize("hasAnyRole('DOCTOR')")
     @PutMapping("/update/{id}")
     public TestRequest updateConsultation(@PathVariable Long id,@RequestBody CreateConsultationRequest testResult) {
-
-        // Implement this method
-
-        // Implement this method to update the result of the current test request id with test doctor comments
-        // Create an object of the User class to get the logged in user
-        // Create an object of TestResult class and make use of updateConsultation() method from testRequestUpdateService class
-        //to update the current test request id with the testResult details by the current user(object created)
-        // For reference check the method updateLabTest() method from LabRequestController class
-
         try {
-            // replace this line of code with your implementation
-            throw new ResponseStatusException(HttpStatus.NOT_IMPLEMENTED,"Not implemented");
+            //Get logged-in user(doctor) related details.
+            User doctor =userLoggedInService.getLoggedInUser();
 
-
+            //Update consultation details & return test details.
+            return testRequestUpdateService.updateConsultation(id,testResult,doctor);
         } catch (ConstraintViolationException e) {
             throw asConstraintViolation(e);
         }catch (AppException e) {
             throw asBadRequest(e.getMessage());
         }
     }
-
-
-
 }
